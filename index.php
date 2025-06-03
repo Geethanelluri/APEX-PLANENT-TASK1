ff207f3693d4014159c035ddd244716842943d2d
Geethanelluri <geetha.nelluri6@gmail.com>
2025-06-03 16:09:55 +0530

    inital commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 echo "<h1>Welcome to Apex Internship</h1>";
 echo "<p>Your PHP environment is working correctly!</p>";
-echo "<p>Current Server Time: " . date("Y-m-d H:i:s") . "</p>";
+echo "<p>Current Server Time: " . date("Y-m-d H:i:s")."</p>";
 ?>
